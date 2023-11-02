429782558282fa5ff99545de9fcc8b04b298a5d9
Bernad0t <147069445+Bernad0t@users.noreply.github.com>
2023-11-02 21:11:16 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task 1.py
@@ -0,0 +1,17 @@
+# TODO Напишите функцию для поиска индекса товара
+def find(list, item):
+    count = 0
+    for i in list:
+        if item == list[count]:
+            return count
+        count += 1
+    return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
